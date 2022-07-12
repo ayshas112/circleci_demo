99df9bd3afb8949099d36698c132104ab0d539c1
Aysha Siddiqua LNU <asiddiqua@shiftleft.io>
2022-07-12 09:18:46 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+#  def sayHello():
+#   print("Python World")
+
+#   if __name__ == '__main__':
+#   sayHello()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ test.py
@@ -0,0 +1,32 @@
+# print("Hello Saba")
+# name = input("What is your name? ")
+# print("My name is :" + name)
+
+# code to print your age
+#   birth_year = input("Enter your birth year")
+#   age = 2020 - int(birth_year)
+#   print("Your age is " + str(age))
+
+# study if statements
+# temp = 10
+#   if temp > 35 :
+#    print("its a hot day")
+#    print("drink plenty of water")
+#   elif temp > 20 :
+#    print("Its a nice day")
+#   else:
+#   print("its a cold day")
+
+# learn while loop
+#   i = 5
+#   while i>=1 :
+#    print(i)
+#    i = i-1
+
+# learn functions
+def add(a, b):
+    return a + b
+
+
+c = add(10, 20)
+print(f"Sum : {c}")
